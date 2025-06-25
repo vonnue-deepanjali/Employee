4f3a8e3de3cb6b37923b7c67ea5b862d188ef219
vonnue-deepanjali <deepanjali.dilish@vonnue.com>
2025-06-25 17:25:37 +0530

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
 import './assets/global.css'
-
 import { createApp } from 'vue'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+const app = createApp(App)
+app.use(vuetify)
+app.mount('#app')
